f6a5211346efad287516c0e19036f969de0aad2f
Sammy <samanthabeltran1993@gmail.com>
2024-09-25 11:51:48 -0500

    completed lab

--- src/components/BurgerStack.jsx
+++ src/components/BurgerStack.jsx
@@ -7,7 +7,7 @@ const BurgerStack = ({stack, removeFromBurger}) => {
       {stack.map((ingredient, index) => (
         <li key={index} style={{ color: ingredient.color }}>
           {ingredient.name}
-          <button onClick={() => removeFromBurger(index)}>X</button>
+          <button onClick={() => removeFromBurger(index)}>x</button>
         </li>
       ))}
     </ul>
